1f6556c07dfd5f61f1092a6e32fc19c447487a63
nkostelov <n.kostelov@gmail.com>
2018-01-17 22:12:37 +0200

    разбиваем список с id городов по 20

--- weather.py
+++ weather.py
@@ -3,7 +3,7 @@
 # import json
 # import os
 import sqlite3
-# import datetime
+import time
 import requests
 
 with open('app.id', 'r', encoding='utf-8') as file:
@@ -15,37 +15,44 @@ elements = []
 country = 'UA'
 with sqlite3.connect('weather.db') as con:
     cur = con.cursor()
-    cur.execute('''
-        create table if not exists weather (
-            id integer not null primary key autoincrement unique,
-            wdescript text,
-            temp real,
-            wspeed real,
-            wdeg real,
-            dt integer,
-            townid integer not null unique
-        )
-    ''')
-    for row in cur.execute('select id from towns where country=\'{}\' limit 20'.format(country)):
-        townid.append(str(*row))
-    url = 'http://api.openweathermap.org/data/2.5/group?id={}&lang=ru&units=metric&appid={}'\
-        .format(','.join(townid), app_id)
+    # cur.execute('''
+    #     create table if not exists weather (
+    #         id integer not null primary key autoincrement unique,
+    #         wdescript text,
+    #         temp real,
+    #         wspeed real,
+    #         wdeg real,
+    #         dt integer,
+    #         townid integer not null unique
+    #     )
+    # ''')
+    for row in cur.execute('select id from towns where country=\'{}\''.format(country)):
+        townid.append(*row)
+if len(townid) % 20 != 0:
+    n = len(townid) // 20 + 1
+else:
+    n = len(townid) // 20
+for i in range(n):
+    print(townid[:20])
+    del townid[:20]
+# url = 'http://api.openweathermap.org/data/2.5/group?id={}&lang=ru&units=metric&appid={}'\
+#         .format(','.join(townid), app_id)
     # jfile = requests.get(url).json()
 
 # with open('group.json', 'r', encoding='utf-8') as jfile:
-data = requests.get(url).json()
-for item in data['list']:
-    for datakey, datavalue in item.items():
-        if datakey == 'weather':
-            for k, v in datavalue[0].items():
-                if k == 'description':
-                    elements.append(v)
-        if datakey == 'main' or datakey == 'wind':
-            for k, v in datavalue.items():
-                if k == 'temp' or k == 'speed' or k == 'deg':
-                    elements.append(v)
-        if datakey == 'dt' or datakey == 'id':
-            elements.append(datavalue)
-    base.append(tuple(elements))
-    elements = []
-print(base)
+# data = requests.get(url).json()
+# for item in data['list']:
+#     for datakey, datavalue in item.items():
+#         if datakey == 'weather':
+#             for k, v in datavalue[0].items():
+#                 if k == 'description':
+#                     elements.append(v)
+#         if datakey == 'main' or datakey == 'wind':
+#             for k, v in datavalue.items():
+#                 if k == 'temp' or k == 'speed' or k == 'deg':
+#                     elements.append(v)
+#         if datakey == 'dt' or datakey == 'id':
+#             elements.append(datavalue)
+#     base.append(tuple(elements))
+#     elements = []
+# print(base)
